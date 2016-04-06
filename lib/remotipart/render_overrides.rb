13c724ac3edd37bbd1c64f05326f27de523a0301
Jaydee <jaydee.ji@kakaocorp.com>
2016-04-06 10:27:34 +0900

    Response body

--- lib/remotipart/render_overrides.rb
+++ lib/remotipart/render_overrides.rb
@@ -14,11 +14,11 @@ module Remotipart
       render_without_remotipart *args
       if remotipart_submitted?
         textarea_body = response.content_type == 'text/html' ? html_escape(response.body) : response.body
-        if response.content_type == 'text/html'
-          response.body = %{<script type=\"text/javascript\">try{window.parent.document;}catch(err){document.domain=document.domain;}</script>#{textarea_body}}
-        else
-          response.body = textarea_body
-        end
+        # if response.content_type == 'text/html'
+        #   response.body = %{<script type=\"text/javascript\">try{window.parent.document;}catch(err){document.domain=document.domain;}</script>#{textarea_body}}
+        # else
+        response.body = textarea_body
+        # end
         # response.content_type = Mime[:html]
       end
       response_body
